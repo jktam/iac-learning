4866e50ece28770348e0fe0cd62d281bdff0aa8b
James Tam <cilantropy@gmail.com>
2023-02-21 23:53:33 -0800

    commit terraform files

---
+++ aws-terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-079f51a7bcca65b92"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
